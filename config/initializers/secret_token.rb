928798f870950ef7694aea7df1a72d25877e9b65
Alvaro Molina <alvaromolinac@gmail.com>
2015-05-20 14:17:23 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apijson::Application.config.secret_key_base = 'a141dbf514a66bcf3a758cbce06d61dc8567887f08c5e0e2a4c031362135b67710fb775f654ea2b897c5a51fd020bfdc449e470d06c4da7a254b3ffb6d5c1895'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Apijson::Application.config.session_store :cookie_store, key: '_apijson_session'
